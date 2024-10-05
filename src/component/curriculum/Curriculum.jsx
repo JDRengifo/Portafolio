8b345d651485d2b7c640e285c8a00c3608c73fe0
JDRengifo <josedariorf@gmail.com>
2024-10-05 20:09:52 -0300

    agregando TA

--- src/component/curriculum/Curriculum.jsx
+++ src/component/curriculum/Curriculum.jsx
@@ -21,6 +21,14 @@ function Curriculum() {
         </div>
         <div className="cvderecha">
           <h2 className="educa2">.- Laboral</h2>
+
+          <br />
+          <p>
+            .- Teacher Asistent. (TA): Acompañamiento de nuevos alumnos.
+            Seguimiento de codigo, control de quejas.
+          </p>
+          <br />
+
           <br />
           <p>.- Full Stack Web Developer. Lachoco-Latera, Bogotá Colombía.</p>
           <p>Diseño y producción de página Web, (Trabajo Independiente)</p>
